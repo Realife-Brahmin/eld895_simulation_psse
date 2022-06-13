f63e6bd0daf5ff2632e9daad5f0628c8f81066e1
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2022-06-13 06:26:12 +0530

    Single Generator chasing run test cases added. Vanilla run duration extended.

--- dynamic_simulator.py
+++ dynamic_simulator.py
@@ -5,7 +5,7 @@ import pssepath
 pssepath.add_pssepath()
 
 import numpy as np
-import psse34
+# import psse34
 import psspy
 import excelpy
 
@@ -27,13 +27,14 @@ progressFile, load_increments, impedanceChanged):
     outfile, progressFile)
 
     psspy.strt(0, outfile)
-    psspy.run(0, 0.99, 1, 1, 0)
+    t_normal_run = 25
+    psspy.run(0, 0.99 + t_normal_run, 1, 1, 0)
 
     import numpy as np
     import random as rn
 
-    t_start = 1.00
-    t_end = 100.00
+    t_start = t_normal_run + 1.00
+    t_end = t_normal_run + 300.00
 
     if system_name == 'ieee9':
 
@@ -122,38 +123,49 @@ progressFile, load_increments, impedanceChanged):
         ierr, current_load3 = psspy.aloadreal(3, 4, "TOTALACT")
         current_load3 = current_load3[0]
 
-        if not impedanceChanged:
-            ierr, totals, moto = psspy.scal_2(1, 1, 0, \
-            [psspy._i, 2, 0, 1, 0], \
-            [change_percent1, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0])
+        ierr, totals, moto = psspy.scal_2(1, 1, 0, \
+        [psspy._i, 2, 0, 1, 0], \
+        [change_percent1, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0])
 
-            ierr, totals, moto = psspy.scal_2(2, 1, 0, \
-            [psspy._i, 2, 0, 1, 0],
-            [change_percent2, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0])
+        ierr, totals, moto = psspy.scal_2(2, 1, 0, \
+        [psspy._i, 2, 0, 1, 0],
+        [change_percent2, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0])
 
-            dp2 = current_load2[0] * change_percent2/100
+        dp2 = current_load2[0] * change_percent2/100
 
-            ierr, totals, moto = psspy.scal_2(3, 1, 0, \
-            [psspy._i, 2, 0, 1, 0], \
-            [change_percent3, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0])
+        ierr, totals, moto = psspy.scal_2(3, 1, 0, \
+        [psspy._i, 2, 0, 1, 0], \
+        [change_percent3, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0])
 
-            dp3 = current_load3[0] * change_percent3/100
+        dp3 = current_load3[0] * change_percent3/100
 
-            if current_load1[0] >= 120 and run_number == 2:
-                dp1 = 0
-            else:
-                dp1 = current_load1[0] * change_percent1/100
-        else:
+        if current_load1[0] >= 120 and run_number == 2:
             dp1 = 0
-            dp2 = 0
-            dp3 = 0
+        else:
+            dp1 = current_load1[0] * change_percent1/100
+
 
         print('dp1 = ', dp1)
-        ierr = psspy.increment_gref(1,'1', dp1*1.8/mbase1)
         print('dp2 = ', dp2)
-        psspy.increment_gref(2,'1', dp2*1.8/mbase2)
         print('dp3 = ', dp3)
-        psspy.increment_gref(3,'1', dp3*1.8/mbase3)
+        dpTotal = dp1 + dp2 + dp3
+
+        if run_number == 6:
+            psspy.increment_gref(1, '1', dpTotal*1.8/mbase1)
+            psspy.increment_gref(2, '1', dp2*0.0/mbase2)
+            psspy.increment_gref(3, '1', dp3*0.0/mbase3)
+        elif run_number == 7:
+            psspy.increment_gref(1, '1', dp1*0.0/mbase1)
+            psspy.increment_gref(2, '1', dpTotal*1.8/mbase2)
+            psspy.increment_gref(3, '1', dp3*0.0/mbase3)
+        elif run_number == 8:
+            psspy.increment_gref(1, '1', dp1*0.0/mbase1)
+            psspy.increment_gref(2, '1', dp2*0.0/mbase1)
+            psspy.increment_gref(1, '1', dpTotal*1.8/mbase3)
+        else:
+            psspy.increment_gref(1, '1', dp1*1.8/mbase1)
+            psspy.increment_gref(2, '1', dp2*1.8/mbase2)
+            psspy.increment_gref(3, '1', dp3*1.8/mbase3)
 
         ierr, loss = psspy.aflowreal(-1, 2, 1, 1, 'PLOSS')
         losses = loss[0]
@@ -170,6 +182,9 @@ progressFile, load_increments, impedanceChanged):
         exitFlag = check_simulation_health(filenameProgress, allowLimitBreach)
         if exitFlag:
             break
+
+        trip = 0
+        ierr = psspy.set_osscan(1, trip)
 
     return impedanceChanged
     psspy.lines_per_page_one_device(2, 10000000)
@@ -191,9 +206,11 @@ datapath=None, outpath=None):
 if __name__ == '__main__':
 
     import psse34  #noqa: F401
+    psse_version = 'xplore'
+    # psse_version = 'full'
     simulation_inputs_folder_name = 'simulation_inputs/'
     simulation_outputs_folder_name = 'simulation_outputs/'
-    run_number = 1;
+    run_number = 8;
     impedanceChanged = 0;
     system_name = 'ieee9'
     # system_name = 'ieee39'
@@ -202,10 +219,23 @@ if __name__ == '__main__':
     allowLimitBreach = 1 #allow machine generations to cross limits
     t_increment = 0.1
     import numpy as np
-    load_increments = np.array([27, 29, 21], dtype = float)
+
+    if run_number == 4:
+        load_increments = np.array([7, 9, 11], dtype = float)
+    elif run_number == 5:
+        load_increments = np.array([33, 35, 27], dtype = float)
+    else:
+        load_increments = np.array([27, 29, 21], dtype = float)
+
 
     if system_name == 'ieee9':
-        chosen_channels = np.arange(1, 114)
+        if psse_version == 'xplore':
+            snpfile_suffix = '_snp3_xplore.snp'
+            chosen_channels = np.arange(1, 42)
+        else:
+            snpfile_suffix = '_snp3_full.snp'
+            chosen_channels = np.arange(1, 114)
+
     elif system_name == 'ieee39':
         chosen_channels = np.arange(1, 140)
     else:
@@ -215,11 +245,12 @@ if __name__ == '__main__':
     savfile = simulation_inputs_folder_name \
     + system_name + '_cnv.sav'
 
+
     # snpfile = system_name + '_snp.snp'
     # snpfile = system_name + '_snp1.snp' #D=0 for all generators
     # snpfile = system_name + '_snp2.snp' #D=0, No stabilizers
     snpfile = simulation_inputs_folder_name + \
-    system_name + '_snp3.snp' #No Governors
+    system_name + snpfile_suffix #No Governors
 
     filename_xlsx = simulation_outputs_folder_name + \
     system_name + \
